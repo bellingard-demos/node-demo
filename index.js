6fe09478c022fd9ab4378c2d403ae228e896977e
Fabrice Bellingard <bellingard@gmail.com>
2023-07-18 16:56:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 module.exports = require('./lib/node-progress');
+const crypto = require('crypto');
+
+function callback(err, pub, priv) {}
+
+var { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 1024,
+  publicKeyEncoding:  { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+},
+callback);
