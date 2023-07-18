cdcc27d5e3667160e9d5a818a1a00570b77bf77c
Fabrice Bellingard <bellingard@gmail.com>
2023-07-18 17:09:33 +0200

    Add some feature

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 module.exports = require('./lib/node-progress');
+const crypto = require('crypto');
+
+// Fake callback
+function callback(err, pub, priv) {}
+
+// Add some feature
+var { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 1024,
+  publicKeyEncoding:  { type: 'spki', format: 'pem' },
+  privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
+},
+callback);
